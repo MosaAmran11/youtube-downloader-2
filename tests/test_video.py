23141bd408bf254f4a5d40852e4577abf7ebcca9
Mosa Amran Alawadhi <mosa.alawadhi@gmail.com>
2025-05-26 23:59:19 +0300

    Remove tests dir

--- tests/test_video.py
+++
@@ -1,6 +0,0 @@
-from youtube_downloader.yt_downloader import YoutubeDownloader
-
-
-url = 'https://music.youtube.com/watch?v=xrSayrw9Nvs&si=Btb3ac-0jitm8t4r'
-vid = YoutubeDownloader(url)
-vid.download_video(resolution='720')
